8497d6daf80991d104d487f43d7baf8dbe80aa64
Tyler <53323488+7axes@users.noreply.github.com>
2019-07-28 19:18:37 -0400

    More test questions

--- questions.js
+++ questions.js
@@ -10,5 +10,77 @@ questions = [
       "caus": 0,
       "hist": 0
     }
+  },
+  {
+    "question": "this is a test of the emergency broadcast system",
+    "effect": {
+      "econ": 0,
+      "cult": 1,
+      "soci": 0,
+      "mora": 0,
+      "biol": 0,
+      "caus": 0,
+      "hist": 0
+    }
+  },
+  {
+    "question": "this is a test of the emergency broadcast system",
+    "effect": {
+      "econ": 0,
+      "cult": 0,
+      "soci": 1,
+      "mora": 0,
+      "biol": 0,
+      "caus": 0,
+      "hist": 0
+    }
+  },
+  {
+    "question": "this is a test of the emergency broadcast system",
+    "effect": {
+      "econ": 0,
+      "cult": 0,
+      "soci": 0,
+      "mora": 1,
+      "biol": 0,
+      "caus": 0,
+      "hist": 0
+    }
+  },
+  {
+    "question": "this is a test of the emergency broadcast system",
+    "effect": {
+      "econ": 0,
+      "cult": 0,
+      "soci": 0,
+      "mora": 0,
+      "biol": 1,
+      "caus": 0,
+      "hist": 0
+    }
+  },
+  {
+    "question": "this is a test of the emergency broadcast system",
+    "effect": {
+      "econ": 0,
+      "cult": 0,
+      "soci": 0,
+      "mora": 0,
+      "biol": 0,
+      "caus": 1,
+      "hist": 0
+    }
+  },
+  {
+    "question": "this is a test of the emergency broadcast system",
+    "effect": {
+      "econ": 0,
+      "cult": 0,
+      "soci": 0,
+      "mora": 0,
+      "biol": 0,
+      "caus": 0,
+      "hist": 1
+    }
   }
 ];
