61e4541bcaeaa145ec1e4c64708cdb2a83b10f75
Tyler <53323488+7axes@users.noreply.github.com>
2019-07-28 18:43:41 -0400

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,14 @@
+ideologies = [
+  {
+    "name": "Test",
+    "stats": {
+      "econ": 50,
+      "cult": 50,
+      "soci": 50,
+      "mora": 50,
+      "biol": 50,
+      "caus": 50,
+      "hist": 50
+    }
+  }
+];
