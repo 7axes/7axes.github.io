f1a684aa7afb543e7fcb16d9ef9ead4555185aa4
Tyler <53323488+7axes@users.noreply.github.com>
2019-07-28 13:16:47 -0400

    Test

---
+++ test.js
@@ -0,0 +1,14 @@
+questions = [
+  {
+    "question": this is a test of the emergency broadcast system
+    "effect": {
+      "econ": 1
+      "cult": 0
+      "soci": 0
+      "mora": 0
+      "biol": 0
+      "caus": 0
+      "hist": 0
+    }
+  }
+]
