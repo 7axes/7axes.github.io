907dace45dfbf51fb36f08dc3e51b7b263e34c6b
Tyler <53323488+7axes@users.noreply.github.com>
2019-07-28 16:13:16 -0400

    Commas

--- questions.js
+++ questions.js
@@ -1,13 +1,13 @@
 questions = [
   {
-    "question": "this is a test of the emergency broadcast system"
+    "question": "this is a test of the emergency broadcast system",
     "effect": {
-      "econ": 1
-      "cult": 0
-      "soci": 0
-      "mora": 0
-      "biol": 0
-      "caus": 0
+      "econ": 1,
+      "cult": 0,
+      "soci": 0,
+      "mora": 0,
+      "biol": 0,
+      "caus": 0,
       "hist": 0
     }
   }
