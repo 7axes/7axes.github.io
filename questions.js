35525812caa656237ad6cd52986afa407d45d53e
Tyler <53323488+7axes@users.noreply.github.com>
2019-07-29 13:39:55 -0400

    Test

--- questions.js
+++ questions.js
@@ -2,13 +2,13 @@ questions = [
   {
     "question": "this is a test of the emergency broadcast system",
     "effect": {
-      "econ": 1,
-      "cult": 0,
-      "soci": 0,
-      "mora": 0,
-      "biol": 0,
-      "caus": 0,
-      "hist": 0
+      "econ": 1
+      ,"cult": 0
+      ,"soci": 0
+      ,"mora": 0
+      ,"biol": 0
+      ,"caus": 0
+      ,"hist": 0
     }
   },
   {
